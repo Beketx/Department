87d846a15961571bb39bcf7e8aeb1feee47f31d6
beketx <beketsk@gmail.com>
2022-01-29 00:51:37 +0600

    DONE

--- department/partner/models.py
+++ department/partner/models.py
@@ -3,22 +3,30 @@ from django.db import models
 from utils.utils import generate_client_id, generate_entity_id, generate_department_id
 from utils.consts import GENDER_CHOICES, TYPE_OF
 
+
 class Client(models.Model):
-    id = models.CharField(primary_key=True, default=generate_client_id, unique=True, max_length=255)
-    mobile_phone = models.IntegerField(unique=True, null=True, blank=True, max_length=25)
+    id = models.CharField(primary_key=True, max_length=50, null=True, blank=True, unique=True)
+    mobile_phone = models.IntegerField('Мобильный номер', unique=True, null=True, blank=True, max_length=25)
     mobile_additional = models.ForeignKey('MobilePhone', on_delete=models.SET_NULL, null=True)
-    surname = models.CharField(max_length=100, null=True)
-    name = models.CharField(max_length=100, null=True)
-    familyname = models.CharField(max_length=100, null=True)
+    surname = models.CharField('Фамилия', max_length=100, null=True)
+    name = models.CharField('Имя', max_length=100, null=True)
+    familyname = models.CharField('Очтчество', max_length=100, null=True)
     created_at = models.DateTimeField('Дата и время регистрации', null=True, blank=True)
     updated_at = models.DateTimeField('Дата и время изменения', null=True, blank=True)
     status_updated_at = models.DateTimeField('Дата и время изменения статуса', null=True, blank=True)
-    status = models.BooleanField(default=False)
-    type_of = models.IntegerField(choices=TYPE_OF)
+    status = models.BooleanField('MobilePhone', default=False)
+    type_of = models.IntegerField('Статус', choices=TYPE_OF)
     email = models.ForeignKey('Email', on_delete=models.CASCADE, null=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    gender = models.CharField('Пол', max_length=1, choices=GENDER_CHOICES)
     timezone = models.ForeignKey('Timezone', null=True, verbose_name='Временная зона', on_delete=models.SET_NULL)
     social_network = models.ForeignKey('SocialAccount', on_delete=models.CASCADE, null=True)
+
+    def save(self):
+        if not self.id:
+            self.id = generate_client_id()
+            while Client.objects.filter(id=self.id).exists():
+                self.id = generate_client_id()
+        super(Client, self).save()
 
 class Timezone(models.Model):
     name = models.CharField('Название', max_length=100, null=True, help_text='Например: `Asia/Almaty`')
@@ -33,40 +41,54 @@ class Timezone(models.Model):
 
 
 class MobilePhone(models.Model):
-    client = models.ForeignKey(Client)
-    mobile_phone = models.CharField()
+    client = models.ForeignKey('Client', on_delete=models.CASCADE, null=True)
+    mobile_phone = models.CharField('Описание', max_length=100, null=True)
 
 class Email(models.Model):
-    client = models.ForeignKey(Client)
-    email = models.EmailField()
-    primary = models.BooleanField()
+    client = models.ForeignKey('Client', on_delete=models.CASCADE, null=True)
+    email = models.EmailField('Почта')
+    primary = models.BooleanField('Основной', default=False)
 
 class SocialAccount(models.Model):
     vk = models.ForeignKey('VKAccount', on_delete=models.CASCADE, null=True)
     fb = models.ForeignKey('FBAccount', on_delete=models.CASCADE, null=True)
-    ok = models.CharField(max_length=255, null=True)
-    instagram = models.CharField(max_length=255, null=True)
-    telegram = models.CharField(max_length=255, null=True)
-    whatsapp = models.CharField(max_length=255, null=True)
-    viber = models.CharField(max_length=255, null=True)
+    ok = models.CharField('OK', max_length=255, null=True)
+    instagram = models.CharField('Insta', max_length=255, null=True)
+    telegram = models.CharField('Tg', max_length=255, null=True)
+    whatsapp = models.CharField('Wp', max_length=255, null=True)
+    viber = models.CharField('Vb', max_length=255, null=True)
 
 class VKAccount(models.Model):
-    social_account = models.ForeignKey()
-    vk_link = models.CharField()
+    social_account = models.ForeignKey('SocialAccount', on_delete=models.CASCADE, null=True)
+    vk_link = models.CharField('Ссылка вк', max_length=100, null=True)
 
 class FBAccount(models.Model):
-    social_account = models.ForeignKey()
-    fb_link = models.CharField()
+    social_account = models.ForeignKey('SocialAccount', on_delete=models.CASCADE, null=True)
+    fb_link = models.CharField('Ссылка фб', max_length=100, null=True)
 
 class LegalEntity(models.Model):
-    id = models.CharField(primary_key=True, default=generate_entity_id, unique=True, max_length=255)
+    id = models.CharField(primary_key=True, max_length=50, null=True, blank=True, unique=True)
     created_at = models.DateTimeField('Дата и время регистрации', null=True, blank=True)
     updated_at = models.DateTimeField('Дата и время изменения', null=True, blank=True)
-    name = models.CharField(max_length=255, null=True)
-    short_name = models.CharField(max_length=20, null=True)
-    inn = models.CharField(max_length=255, null=True)
-    kpp = models.CharField(max_length=255, null=True)
+    name = models.CharField('Название', max_length=255, null=True)
+    short_name = models.CharField('Короткое имя', max_length=20, null=True)
+    inn = models.CharField('ИНН', max_length=255, null=True)
+    kpp = models.CharField('КПП', max_length=255, null=True)
+
+    def save(self):
+        if not self.id:
+            self.id = generate_entity_id()
+            while LegalEntity.objects.filter(id=self.id).exists():
+                self.id = generate_entity_id()
+        super(LegalEntity, self).save()
 
 class Department(models.Model):
-    id = models.CharField(primary_key=True, default=generate_department_id, unique=True, max_length=255)
-    name = models.CharField(max_length=255, null=True)
+    id = models.CharField(primary_key=True, max_length=50, null=True, blank=True, unique=True)
+    name = models.CharField('Имя', max_length=255, null=True)
+
+    def save(self):
+        if not self.id:
+            self.id = generate_department_id()
+            while Department.objects.filter(id=self.id).exists():
+                self.id = generate_department_id()
+        super(Department, self).save()
